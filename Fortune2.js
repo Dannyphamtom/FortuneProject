fa69e741cb956db28cef211f5502f5fd50a6b986
Dannyphamtom <dannypham.dp02@gmail.com>
2024-05-11 00:01:49 -0500

    updates

--- Fortune2.js
+++ Fortune2.js
@@ -1,19 +1,20 @@
-// Function to fetch the fortune when the button is clicked
 document.getElementById('get-fortune-btn').addEventListener('click', function() {
-    // Make an HTTP GET request to your API endpoint
     fetch('https://ywtzp7u3v9.execute-api.us-east-1.amazonaws.com/Test/Fortune-Lambda')
-        .then(response => response.json()) // Parse the JSON response
+        .then(response => response.json())
         .then(data => {
-            const fortune = data.Fortunes.S;
-            // Update the fortune container with the fetched fortune
-            document.getElementById('fortune-container').innerHTML = `<p>${fortune}</p>`;
+            const fortune = data.Fortunes && data.Fortunes.S; // Check if Fortunes exists before accessing S
+            if (fortune) {
+                document.getElementById('fortune-container').innerHTML = `<p>${fortune}</p>`;
+            } else {
+                document.getElementById('fortune-container').innerHTML = `<p>No fortune available</p>`;
+            }
         })
         .catch(error => {
-            // Handle any errors
             console.error('Error fetching fortune:', error);
             document.getElementById('fortune-container').innerHTML = `<p>Error fetching fortune</p>`;
         });
 });
+
 
 // Function to submit a fortune when the button is clicked
 document.getElementById('submit-fortune-btn').addEventListener('click', function() {
